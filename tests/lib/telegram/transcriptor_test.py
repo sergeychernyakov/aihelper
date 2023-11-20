f3adc1429db1e9920a3e837d356e0cd7004805b7
Sergey Chernyakov <chernyakov.sergey@gmail.com>
2023-11-21 00:04:13 +0200

    added answer_test

---
+++ tests/lib/telegram/transcriptor_test.py
@@ -0,0 +1,54 @@
+import unittest
+from unittest.mock import Mock, patch, mock_open
+from lib.telegram.transcriptor import Transcriptor
+
+class TestTranscriptor(unittest.TestCase):
+
+    def setUp(self):
+        # Mock dependencies
+        self.mock_openai_client = Mock()
+        self.mock_update = Mock()
+        self.mock_context = Mock()
+        self.mock_bot = Mock()
+        self.mock_update.message = Mock(caption=None, chat_id='chat_id')
+        self.mock_context.bot = self.mock_bot
+
+        # Setup for Transcriptor instance
+        self.transcriptor = Transcriptor(self.mock_openai_client, self.mock_update, self.mock_context, 'thread_id', 'assistant_id')
+
+    @patch('builtins.open', new_callable=mock_open)
+    def test_transcript_document(self, mock_file):
+        file_path = "path/to/document"
+        self.transcriptor.transcript_document(file_path)
+
+        mock_file.assert_called_once_with(file_path, "rb")
+        self.mock_openai_client.files.create.assert_called_once()
+        self.mock_openai_client.beta.threads.messages.create.assert_called_once()
+
+    def test_transcript_image(self):
+        file = Mock(file_path="path/to/image")
+        mock_response = Mock()
+        mock_response.choices = [Mock(message=Mock(content="Mocked Content"))]
+        self.mock_openai_client.chat.completions.create.return_value = mock_response
+        
+        self.transcriptor.transcript_image(file)
+
+        self.mock_openai_client.chat.completions.create.assert_called_once()
+        self.mock_bot.send_message.assert_called_once_with('chat_id', "Mocked Content")
+        self.mock_openai_client.beta.threads.messages.create.assert_called_once()
+
+
+    @patch('builtins.open', new_callable=mock_open)
+    def test_transcript_voice(self, mock_file):
+        file_path = "path/to/audio"
+        self.mock_openai_client.audio.transcriptions.create.return_value = "Mocked Transcription"
+        
+        self.transcriptor.transcript_voice(file_path)
+
+        mock_file.assert_called_once_with(file_path, "rb")
+        self.mock_openai_client.audio.transcriptions.create.assert_called_once()
+        self.mock_bot.send_message.assert_called_once_with('chat_id', "Mocked Transcription")
+        self.mock_openai_client.beta.threads.messages.create.assert_called_once()
+
+if __name__ == '__main__':
+    unittest.main()
